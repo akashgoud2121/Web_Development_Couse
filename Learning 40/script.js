8d477e60a7491bdf56f8b41a975931aef22b9a61
Akash <letsmail.akashgoud@gmail.com>
2025-02-18 11:39:14 +0530

    Javascript Introduction

---
+++ Learning 40/script.js
@@ -0,0 +1,17 @@
+alert("Hello World!")
+console.log("code is running...")
+
+
+var a = prompt("Enter your number")
+var isTrue=confirm("Are you sure you want to blast your computer")
+
+if(isTrue){
+    console.log("computer is blasting")
+
+    
+}
+else{
+    console.log("computer is not blasting")
+
+}
+console.log("your number is "+a)
