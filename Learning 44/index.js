479d7483ab763214425276e5f940c39cca97bdc6
Akash <letsmail.akashgoud@gmail.com>
2025-02-18 13:35:48 +0530

    Js Functions

---
+++ Learning 44/index.js
@@ -0,0 +1,27 @@
+function nice(name){
+    console.log("Hey " + name + " you are good!");
+    console.log("Hey " + name +" you are awesome!");
+    console.log("Hey " + name +" you are insame!");
+}
+
+
+
+nice("Akash");
+nice("Rohan");
+
+function sum(a,b ,c=3){
+    // console.log(a+b);
+    return a+b+clearInterval;
+}
+
+result=sum(3,5);
+
+
+console.log("THE sum of these numbers is "+ result);
+
+const func1 =(x)=>{
+    console.log("I am an arrow function",x)
+    
+} 
+func1(34)
+func2(66)
