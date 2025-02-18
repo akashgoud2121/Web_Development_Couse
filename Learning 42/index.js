8c2248d371fd4c6fd4e245cf7f3d97e81dfd289f
Akash <letsmail.akashgoud@gmail.com>
2025-02-18 12:41:21 +0530

    Js Conditional STATEMENTS

---
+++ Learning 42/index.js
@@ -0,0 +1,31 @@
+console.log("Hello this learning is about conditional tutorial")
+
+let age=15;
+let grace=5;
+
+age+=grace
+console.log(age)
+
+if((age+grace)==18){
+    console.log("you can vote")
+}
+
+else if(age==0){
+    console.log("Are you kidding")
+}
+
+else{
+    console.log("you cannot drive")
+}
+
+
+console.log(age + grace)
+console.log(age - grace)
+console.log(age * grace)
+console.log(age / grace)
+console.log(age % grace)
+console.log(age ** grace)
+
+
+
+
