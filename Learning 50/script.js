ef80dc3f0b4faca0782ff8e4321a9b7591ed50f6
Akash <letsmail.akashgoud@gmail.com>
2025-02-22 18:56:47 +0530

    JS Dom Parent & Siblings

---
+++ Learning 50/script.js
@@ -0,0 +1,25 @@
+document.body
+​
+document.body.childNodes
+
+document.body.childNodes[0]
+document.body.childNodes[1]
+document.body.childNodes[1].childNodes
+let cont =document.body.childNodes[1]
+
+cont
+cont.firstChild
+
+cont.lastChild
+cont.firstElementChild
+cont.lastElementChild
+cont.lastElementChild.style.color="red"
+'red'
+cont.lastElementChild.style.backgroundColor="yellow"
+'yellow'
+cont.lastElementChild.parentElement
+document.body.firstElementChild
+document.body.firstElementChild.childNodes
+NodeList(11) [text, div.box, text, div.box, text, div.box, text, div.box, text, div.box, text]
+document.body.firstElementChild.children
+HTMLCollection(5) [div.box, div.box, div.box, div.box, div.box]
